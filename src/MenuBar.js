7b781655b2c3f1e3424033465c25af34cb44776a
Andrew Esteban <andrewesteban1999@gmail.com>
2020-07-15 18:04:55 +1000

    Updated menubar

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
 import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 
 export default class MenuBar extends React.Component {
 
@@ -18,42 +17,42 @@ export default class MenuBar extends React.Component {
 
     render() {
         return (
-            <AppBar position="static" style={{ backgroundColor: "#FFFFFF", height: "7vh", width: "1", color: "#000000" }}>
-                <Toolbar>
+            <AppBar position="static" style={{ backgroundColor: "#FFFFFF", height: "7vh", width: "100%", color: "#000000", paddingTop: "1vh", minHeight: "50px"}}>
                     <Grid container>
                         <Grid item xs={4}>
                             <img
                                 src="koala.jpg"
                                 className="App-logo"
                                 alt="logo"
-                                padding="5"
+                                style={{height: "5vh", paddingLeft: "1%"}}
                             />
-                            <Typography variant="h3" style={{ display: "inline" }}>
+                            <Typography variant="h3" style={{ display: "inline", paddingLeft: "2%", paddingBotton: "10%", verticalAlign: "super", fontSize: "calc(12px + 3vh)" }}>
                                 <strong>Business Buddy</strong>
                             </Typography>
                         </Grid>
 
-                        
-                        <Grid item xs={4}>
-                            <Button>HOME</Button>
-                            <Button>PRIVACY</Button>
-                        </Grid>
 
                         <Grid item xs={4}>
-                            <Grid container>
-                                <Grid item xs={10}>
-                                    <Typography variant="h5" align="right">
-                                        {this.state.name}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={1}></Grid>
-                                <Grid item xs={1}>
-                                    <Avatar alt="Cindy Baker" src="pizza.jpg" />
-                                </Grid>
-                            </Grid>
+                            <Button size="large" style={{marginLeft: "1%", marginTop: "1%"}}>HOME</Button>
+                            <Button size="large" style={{marginLeft: "1%", marginTop: "1%"}}>PRIVACY</Button>
+                        </Grid>
+
+                        <Grid item xs={4} style={{textAlign: "right"}}>
+                            <Box>
+                            <Typography variant="h5" style={{ display: "inline", paddingLeft: "2%", fontSize: "calc(12px + 1vh)" }}>
+                                {this.state.name.toUpperCase()}
+                            </Typography>
+
+                            <img
+                                src="pizza.jpg"
+                                className="App-logo"
+                                alt="company"
+                                style={{height: "5vh", paddingLeft: "1%"}}
+                            />
+                            </Box>
                         </Grid>
                     </Grid>
-                </Toolbar>
+                {/* </Toolbar> */}
             </AppBar>
 
         );
